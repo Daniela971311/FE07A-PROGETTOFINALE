859c211011273424fb8c4ff887ce4313b0129f8b
Daniela971311 <98736109+Daniela971311@users.noreply.github.com>
2022-03-21 16:36:58 +0100

    Update my-http-interceptor.interceptor.ts
    token aggiornato

--- src/app/_interceptors/my-http-interceptor.interceptor.ts
+++ src/app/_interceptors/my-http-interceptor.interceptor.ts
@@ -11,7 +11,7 @@ import { Observable } from 'rxjs';
 export class MyHttpInterceptorInterceptor implements HttpInterceptor {
   tenantID = 'fe_0721a';
   bearerToken =
-    'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY0NjkzMzM4NCwiZXhwIjoxNjQ3Nzk3Mzg0fQ.8aqaeWa5MADEqp4VM4kqTqWd0zQMszaaWnxYv9mGoHHJGdblHR8e7Jpd6G1gxMDJbVRftLmlqOADvn51R1sWrw';
+    'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY0NzUwNzE5OCwiZXhwIjoxNjQ4MzcxMTk4fQ.hDmA5CnOrvWrGOq_cOiMaptYKXUvU186b5BoxNRqjLrDfpByPrdIacV0GFkBA_U7lWVqK4LekBVXXAskE_ft6Q';
 
   constructor() {}
 
